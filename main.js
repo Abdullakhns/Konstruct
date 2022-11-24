516c0fbbeb6c926d1279b941d787b4756eeb5330
Abdullakhns <mr.abdulloh98@gmail.com>
2022-11-24 14:01:56 +0500

    created page

---
+++ main.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $('.menu-btn').click(function() {
+        $('.container .nav-links').toggleClass("active")
+        $('.menu-btn i').toggleClass("active")
+    })
+})
